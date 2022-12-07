7c6cc371aa53f8b07515e5ce9085692a46bbf79c
juliaebert2903 <47864810+juliaebert2903@users.noreply.github.com>
2022-12-06 22:43:38 -0300

    Add files via upload

---
+++ elementos-controller.js
@@ -0,0 +1,92 @@
+const { Elemento } = require('../models/elementos-model')
+const { validateElemento, validateGetElementos, validateGetElemento } = require('../validators/elementos-validator');
+const { Personagem } = require('../models/personagens-model');
+
+class ElementosController {
+    constructor() {}
+
+    async create(req, res) {
+
+        const elementosBody = req.body;
+        try {
+            const invalido = await validateElemento(elementosBody);
+            if (invalido) {
+                throw {
+                    status: 400,
+                    message: invalido.details[0].message
+                }
+            }
+            const ex = {
+                nome: elementosBody.nome
+            }
+
+            await Elemento.create(ex);
+            return res.status(200).json(ex);
+
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    }
+
+    async detail(req, res) {
+        try {
+            const { id } = req.params;
+            /*const invalido = await validateGetElemento({ id });
+            if (invalido) {
+                throw {
+                    status: 400,
+                    message: invalido.details[0].message
+                }
+            }*/
+            const elemento = await Elemento.findByPk(id)
+            res.status(200).json(elemento);
+        } catch (err) {
+            return res.status(400).json({ err });
+        }
+    }
+
+    async listaElementos(req, res) {
+        const invalido = await validateGetElementos(req.query)
+        if (invalido) {
+            throw {
+                status: 400,
+                message: invalido.details[0].message
+            }
+        }
+        try {
+            const elementos = await Elemento.findAndCountAll({
+                limit: 10,
+                offset: 0
+            })
+            res.status(200).json(elementos);
+        } catch (err) {
+            return res.status(400).json({ err });
+        }
+    }
+
+    async listaPersonagemPorElemento(req, res) {
+        try {
+            const { id } = req.params;
+            const invalido = await validateGetElemento({ id });
+            if (invalido) {
+                throw {
+                    status: 400,
+                    message: invalido.details[0].message
+                }
+            }
+            const personagens = await Personagem.findAndCountAll({
+                where: {
+                    id: id
+                },
+                include: [
+                    { model: Elemento }
+                ]
+            })
+            res.status(200).json(personagens);
+        } catch (err) {
+            return res.status(400).json({ err });
+        }
+    }
+}
+
+module.exports = ElementosController
